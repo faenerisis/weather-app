0987a6087c0313ccb06a414fc71153c2d734010d
faenerisis <67673597+faenerisis@users.noreply.github.com>
2022-12-14 14:21:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', ()=> {
+    let long; //longitude
+    let lat; //latitude
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimzezone = document.querySelector('.location-timezone');
+
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=current&appid={f2a693adf23ca3ae36fa81199ea99523}`;
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const { temperature, summary } = data.currently;
+                temperatureDegree.textContent = temperature;
+                temperatureDescription = summary;
+            })
+        });
+    }
+});
